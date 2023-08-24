dc003b7abcc9347471a914f382b94063393292c3
Insoo Baek <baikinsoo95@gmail.com>
2023-08-24 18:33:26 +0900

    2023.08.24.THU cyworld 미니홈피 만들기 ~ 1차 완성

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,22 +1,37 @@
+let isStarted = false;
+
 let send = () => {
-    let token = String(Math.floor(Math.random()*100000)).padStart(6,"0")
-    document.getElementById("number").innerText = token
 
-    let time = 3
+    if (isStarted === false) {
+        // 타이머가 작동중이 아닐때
+        isStarted = true
 
-    setInterval(function () {
-        if (time >= 0) {
-            let min = Math.floor(time/60)
-            let sec = String(time%60).padStart(2,"0")
-            document.getElementById("timer").innerText = min + ":" + sec
-            time = time - 1
-        } else {
-            document.getElementById("finish").disabled = true
-        }
-    },1000)
+        document.getElementById("finish").disabled = false
+
+        let token = String(Math.floor(Math.random()*100000)).padStart(6,"0");
+        document.getElementById("number").innerText = token
+
+        let time = 10
+        let timer
+
+        timer = setInterval(function () {
+            //
+
+            if (time >= 0) {
+                let min = Math.floor(time/60)
+                let sec = String(time%60).padStart(2,"0")
+                document.getElementById("timer").innerText = min + ":" + sec
+                time = time - 1
+            } else {
+                document.getElementById("finish").disabled = true
+                isStarted = false
+                console.log("타이머 작동중")
+                clearInterval(timer)
+            }
+        },1000)
+
+    } else {
+        // 타이머가 작동중일 때
+    }
 }
 
-let stop = () => {
-
-    document.getElementById("timer").innerText = "00" + ":" + "00"
-}
---
+++ cyworld/index.js
@@ -0,0 +1,22 @@
+const menuHome = () => {
+    document.getElementById("contentFrame").setAttribute("src","home.html")
+//     매개변수가 2개 들어간다. "src", "값"
+    document.getElementById("menuHome").style = "color: black; background-color: white"
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298eb5"
+    document.getElementById("menuGame").style = "color: white; background-color: #298eb5"
+
+};
+
+const menuJukebox = () => {
+    document.getElementById("contentFrame").setAttribute("src","jukebox.html")
+    document.getElementById("menuJukebox").style = "color: black; background-color: white"
+    document.getElementById("menuHome").style = "color: white; background-color: #298eb5"
+    document.getElementById("menuGame").style = "color: white; background-color: #298eb5"
+};
+
+const menuGame = () => {
+    document.getElementById("contentFrame").setAttribute("src","game.html")
+    document.getElementById("menuGame").style = "color: black; background-color: white"
+    document.getElementById("menuHome").style = "color: white; background-color: #298eb5"
+    document.getElementById("menuJukebox").style = "color: white; background-color: #298eb5"
+};
